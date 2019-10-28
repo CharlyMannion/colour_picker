6b12dd54ff20d86090be6ae1843299ae311da6ab
Charlotte Mannion <charlottemannion100@gmail.com>
2019-10-28 10:31:41 +0000

    writes add box function and rearranges script

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 /* Exercise 2: Color picker */
+$(document).on('keyup', '#color', function() {
+    setPreviewColor($('#color').val());
+});
+
 function setPreviewColor(inputColor) {
     $('.preview').css('background-color', inputColor);
     $('.color-code').text(inputColor);
+};
+
+function addBox(color) {
+    var newBox = '<div class="item" style="background-color: ' + color + ';"></div>';
+    $('#colors').prepend(newBox);
 }
 
-$(document).on('keyup', '#color', function() {
-    setPreviewColor($('#color').val());
-})
