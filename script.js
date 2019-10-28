275637486e3a2329b60dab02cda5809fdb72d624
Charlotte Mannion <charlottemannion100@gmail.com>
2019-10-28 10:26:12 +0000

    sets input colour text to inputColor variable

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /* Exercise 2: Color picker */
 function setPreviewColor(inputColor) {
     $('.preview').css('background-color', inputColor);
+    $('.color-code').text(inputColor);
 }
 
 $(document).on('keyup', '#color', function() {
